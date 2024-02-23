4135b28946a422a648781dde8afca6dd0e1f57d2
amyheather <a.heather2@exeter.ac.uk>
2024-02-23 09:16:35 +0000

    Add create symbol report function (work in progress)

--- kailo_beewell_dashboard/static_report.py
+++ kailo_beewell_dashboard/static_report.py
@@ -224,3 +224,159 @@ style='width:650px; height:192px;'>'''
 '''
 
     return html_content
+
+
+def create_static_symbol_report(chosen_school, chosen_group, df_prop, counts,
+                                dem_prop, pdf_title):
+    '''
+    Generate a static symbol survey PDF report for the chosen school and group,
+    with all the key information and figures from the dashboard
+
+    Parameters
+    ----------
+    chosen_school : string
+        Name of the chosen school
+    chosen_group : string
+        Name of the chosen group to view results by - options are
+        'For all pupils', 'By year group', 'By gender', 'By FSM' or 'By SEN'
+    df_prop : dataframe
+        Dataframe with proportion of each response to each survey question
+    counts : dataframe
+        Dataframe with the counts of pupils at each school
+    dem_prop : dataframe
+        Dataframe with proportion of each reponse to the demographic questions
+    pdf_title : string
+        Title for the PDF file
+    '''
+    ##########
+    # Set-up #
+    ##########
+    # Create empty list to fill with HTML content for PDF report
+    content = []
+
+    # Get list of topics to use for table of contents
+    survey_topics = df_prop['measure_lab'].drop_duplicates()
+    dem_topics = dem_prop['measure_lab'].drop_duplicates()
+
+    # Get school size
+    # school_size = get_school_size(counts, chosen_school, 'symbol')
+
+    ##############
+    # Title page #
+    ##############
+
+    # Logo - convert to HTML, then add to the content for the report
+    data_uri = base64.b64encode(open('images/kailo_beewell_logo_padded.png',
+                                     'rb').read()).decode('utf-8')
+    img_tag = f'''
+<img src='data:image/png;base64,{data_uri}' alt='Kailo #BeeWell logo'
+style='width:300px; height:182px;'>'''
+    content.append(img_tag)
+
+    # Get group name with only first character modified to lower case
+    group_lower_first = chosen_group[0].lower() + chosen_group[1:]
+
+    # Title and introduction
+    title_page = f'''
+<div class='section_container'>
+    <h1 style='text-align:center;'>The #BeeWell Survey</h1>
+    <p style='text-align:center; font-weight:bold;'>Thank you for taking
+    part in the #BeeWell survey delivered by Kailo.</p>
+    <p>The results from pupils at your school can be explored using the
+    interactive dashboard at
+    https://synthetic-beewell-kailo-standard-school-dashboard.streamlit.app/.
+    This report has been downloaded from that dashboard.<br><br>
+    There are four reports available - these have results: (a) from all
+    pupils, (b) by gender, (c) by free school meal (FSM) eligibility, and
+    (d) by year group.<br><br>
+    This report contains the results <b>{group_lower_first}</b> for
+    <b>{chosen_school}</b>.</p>
+</div>
+'''
+    content.append(title_page)
+
+    # Illustration - convert to HTML, then add to the content for the report
+    data_uri = base64.b64encode(open('images/home_image_3_transparent.png',
+                                     'rb').read()).decode('utf-8')
+    img_tag = f'''
+<img src='data:image/png;base64,{data_uri}' alt='Kailo illustration'
+style='width:650px; height:192px;'>'''
+    illustration = f'''
+<div style='width:100%; position:absolute; bottom:0;'>
+    {img_tag}
+</div>'''
+    content.append(illustration)
+
+    ################
+    # Introduction #
+    ################
+
+    # Heading
+    content.append('''<h1 style='page-break-before:always;'>
+                   Introduction</h1>''')
+
+    # Using the report (duplicate text with About.py)
+    content.append('<h2>How to use this report</h2>')
+    content.append(markdown(text_how_use()))
+
+    #####################
+    # Table of contents #
+    #####################
+
+    # Get all of the explore results pages as lines for the table of contents
+    explore_results_pages = []
+    for key, value in survey_topics.items():
+        line = f'''<li><a href='#{value}'>{key}</a></li>'''
+        explore_results_pages.append(line)
+
+    # Get the demographic headers as lines for the table of contents
+    demographic_pages = []
+    for key, value in dem_topics.items():
+        line = f'''<li><a href='#{key}'>{value}</a></li>'''
+        demographic_pages.append(line)
+
+    content.append(f'''
+<div>
+    <h1 style='page-break-before:always;'>Table of Contents</h1>
+    <ul>
+        <li><a href='#explore_results'>Explore results</a> - Explore how
+            your pupils responded to each survey question
+            <ul>{''.join(explore_results_pages)}</ul>
+        </li>
+        <br>
+        <li><a href='#who_took_part'>Who took part</a> - See the
+            characteristics of the pupils who took part in the survey
+            <ul>{''.join(demographic_pages)}</ul>
+        </li>
+    </ul>
+</div>
+''')
+
+    ######################
+    # Create HTML report #
+    ######################
+
+    # Remove the final temporary image file
+    if os.path.exists('report/temp_image.png'):
+        os.remove('report/temp_image.png')
+
+    # Import the CSS stylesheet
+    with open('css/static_report_style.css') as css:
+        css_style = css.read()
+
+    html_content = f'''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{pdf_title}</title>
+    <style>
+        {css_style}
+    </style>
+</head>
+<body>
+    {''.join(content)}
+</body>
+</html>
+'''
+
+    return html_content
